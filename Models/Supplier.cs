f34c0db031947b5a9f76ebae9df256f9d486aba5
Isigarcia0493 <ig705295@southeast.edu>
2023-03-05 10:23:03 -0600

    supplier model updated

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,27 +9,43 @@ namespace CapstoneGroupProject.Models
 {
     public class Supplier
     {
+        [Key]
         public int SupplierID { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter a supplier name")]
         [StringLength(100)]
-        [Display(Name = "Supplier Name")]
+        [DisplayName("Supplier Name")]
         public string SupplierName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter the supplier address")]
         [StringLength(100)]
-        [Display(Name = "Supplier Address")]
+        [DisplayName("Address")]
         public string Address { get; set; }
 
-        [Required]
-        [StringLength(11)]
-        [Display(Name = "Supplier Phone Number")]
+        [Required(ErrorMessage = "Enter city")]
+        [StringLength(100)]
+        [DisplayName("City")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Enter State")]
+        [StringLength(20)]
+        [DisplayName("State")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Enter Zip Code")]
+        [DisplayName("Zip Code")]
+        public int ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Enter a phone number")]
+        [StringLength(14)]
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
 
-        [StringLength(25)]
-        [Display(Name = "Supplier Email")]
+        [Required(ErrorMessage = "Enter an email")]
+        [StringLength(50)]
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
-
-
     }
 }
