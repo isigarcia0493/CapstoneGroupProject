008e64eb5a3ae895b42116f280016649e46b0e26
Zane Morgan <zm469692@southeast.edu>
2023-03-06 14:31:26 -0600

    Added employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneGroupProject.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        //Relationships
+        [ForeignKey("ID")]
+        public string ID { get; set; }
+
+    }
+}
