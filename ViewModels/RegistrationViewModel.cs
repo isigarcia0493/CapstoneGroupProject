4f0252fabb4b308f81977e8958571fc8aac66a55
Isai Gonzalez Garcia <ig705295@southeast.edu>
2023-03-06 14:39:42 -0600

    Login, Register, logout working properly

---
+++ ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneGroupProject.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "Please enter email")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        [DisplayName("Email")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DisplayName("Confirm Password")]
+        [Compare("Password", ErrorMessage = "The confirmation password does not match the password")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
