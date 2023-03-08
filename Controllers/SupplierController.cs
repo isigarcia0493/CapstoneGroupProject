98e90559764c181b887bd1a81766d2119dd931a6
Zane Morgan <fremen925@yahoo.com>
2023-03-07 22:33:23 -0600

    Added supplier view and controller

---
+++ Controllers/SupplierController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CapstoneGroupProject.Models;
+using CapstoneGroupProject.ViewModels;
+using Microsoft.IdentityModel.Protocols;
+
+namespace CapstoneGroupProject.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: SupplierController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SupplierController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SupplierController/Create
+        public ActionResult CreateSupplier()
+        {
+            return View();
+        }
+
+        // POST: SupplierController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateSupplier(SupplierViewModel vmSupplier)
+        {
+            if (ModelState.IsValid)
+            {
+                Supplier supplier = new Supplier();
+                supplier.SupplierName = vmSupplier.SupplierName;
+                supplier.Address = vmSupplier.Address;
+                supplier.City = vmSupplier.City;
+                supplier.Email = vmSupplier.Email;
+                supplier.PhoneNumber = vmSupplier.PhoneNumber;
+                supplier.State = vmSupplier.State;
+                supplier.ZipCode = vmSupplier.ZipCode;
+
+                // Send to DB
+                
+            }
+            return View();
+        }
+
+        // GET: SupplierController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SupplierController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SupplierController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SupplierController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ViewModels/SupplierViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneGroupProject.ViewModels
+{
+    public class SupplierViewModel
+    {
+        [Key]
+        public int SupplierID { get; set; }
+
+        [Required(ErrorMessage = "Enter a supplier name")]
+        [StringLength(100)]
+        [DisplayName("Supplier Name")]
+        public string SupplierName { get; set; }
+
+        [Required(ErrorMessage = "Enter the supplier address")]
+        [StringLength(100)]
+        [DisplayName("Address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Enter city")]
+        [StringLength(100)]
+        [DisplayName("City")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Enter State")]
+        [StringLength(20)]
+        [DisplayName("State")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Enter Zip Code")]
+        [DisplayName("Zip Code")]
+        public int ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Enter a phone number")]
+        [StringLength(14)]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Enter an email")]
+        [StringLength(50)]
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+    }
+}
