f31692aa50ba36d8b11240ae203411406c3e1e92
Isai Gonzalez Garcia <ig705295@southeast.edu>
2023-03-20 14:25:07 -0500

    Employee controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using CapstoneGroupProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneGroupProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _appDbContext.Employees.ToList();
+
+            return View(employees);
+        }
+    }
+}
