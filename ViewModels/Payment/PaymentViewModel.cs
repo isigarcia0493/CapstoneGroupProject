75d17d02e16c1b6f217ad58e3eb0722566075ac3
Zane Morgan <zm469692@southeast.edu>
2023-03-27 14:34:38 -0500

    Changing background and finishing up supplier views. Added category and payment viewmodels for future use.

---
+++ ViewModels/Payment/PaymentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using CapstoneGroupProject.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneGroupProject.ViewModels
+{
+    public class PaymentViewModel
+    {
+        [Key]
+        public int PaymentID { get; set; }
+
+        [Required(ErrorMessage = "Enter a payment Type")]
+        [StringLength(50)]
+        [DisplayName("Payment Type")]
+        public string PaymentType { get; set; }
+
+        //Relationships
+        public int OrderID { get; set; }
+        [ForeignKey("OrderID")]
+        public Models.Order Order { get; set; }
+    }
+}
